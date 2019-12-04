8c4a7cfc6af05cda5240d48d4d304d717b84e5a0
your name <jackwillee1995@gmail.com>
2019-12-04 13:48:42 +0000

    interactive menu added

--- directory.rb
+++ directory.rb
@@ -11,6 +11,28 @@ def input_students
   end
   students
 end
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -24,6 +46,7 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+interactive_menu
 students = input_students
 print_header
 print(students)
